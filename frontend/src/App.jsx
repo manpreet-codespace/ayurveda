d89a6956e464eafa6f9e48e7585b79065025a7e5
manpreet-codespace <manpreet.dev.85@gmail.com>
2026-04-07 22:55:55 +0530

    first commit

---
+++ frontend/src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react"
+import Navbar from "./Components/Layouts/Navbar"
+
+function App() {
+
+  return (
+    <>
+      <Navbar/>
+    </>
+  )
+}
+
+export default App
---
+++ frontend/src/Components/Layouts/Navbar.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <>
+    <nav className='w-full border'>
+        <div className='w-full ' >
+            <a href='tel:+91770797059' className='bg-[var(--brown)] text-white text-[26px] font-semibold'>CALL NOW +91 77079 77059</a>
+        </div>
+    </nav>
+    </>
+  )
+}
+
+export default Navbar
