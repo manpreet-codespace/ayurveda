bdd5ef41c5a97dfc5d10262ebb9a9310195829ff
manpreet-codespace <manpreet.dev.85@gmail.com>
2026-06-04 00:23:52 +0530

    new: admin product page

--- frontend/src/admin/Pages/Product.jsx
+++ frontend/src/admin/Pages/Product.jsx
@@ -1,14 +1,20 @@
 import JoditEditor from 'jodit-react';
 import React, { useRef, useState } from 'react'
 import { toolbarConfig } from '../../config/toolbarConfig';
+import EditButton from '../Components/EditButton';
+import DeleteButton from '../Components/DeleteButton';
+import { FaChevronDown, FaChevronUp } from 'react-icons/fa6';
 
 const Product = () => {
   const editorRef = useRef(null);
   const [content, setContent] = useState("");
 
+  const [open,setOpen] = useState(false);
+
+
   return (
     <>
-      <section className='bg-white p-3 w-11/12 mx-auto rounded-lg '>
+      <section className='bg-white p-3 w-11/12 mx-auto rounded-lg'>
         <h1 className='text-[20px] font-semibold'>Categories of Products</h1>
         <input type="text" placeholder='Write categories' className='border border-gray-200 w-50 p-2 m-2' />
         <button className='bg-black py-2 px-4 text-white rounded-lg tracking-wider font-semibold'>Save</button>
@@ -73,13 +79,85 @@ const Product = () => {
 
         </div>
 
-          <div className='mt-10 flex justify-right '>
-            <button className='bg-black text-white py-2 px-4 tracking-wider font-semibold rounded-lg'>
-              Save
-              </button>
+        <div className='mt-10 flex justify-end'>
+          <button className='bg-black text-white py-2 px-4 tracking-wider font-semibold rounded-lg'>
+            Save
+          </button>
+        </div>
 
-            </div>
+      </section>
+      <br />
+      <section className='bg-white p-3 w-11/12 mx-auto rounded-lg mb-5'>
+        <h1 className='text-[20px] font-semibold'>Stock of Products</h1>
 
+        <div className='flex gap-4 mt-2 '>
+          <label htmlFor='product'>Products</label>
+          <select name='product'>
+            <option value="none">--Select--</option>
+          </select>
+        </div>
+
+        <div className='flex mt-2 gap-4 items-center'>
+          <label htmlFor='stock'>Stocks</label>
+          <input type="number" name="stock" placeholder='Update stock here ' className='border border-gray-200 p-2 ' />
+        </div>
+
+        <div className='mt-10 flex justify-end'>
+          <button className='bg-black text-white py-2 px-4 tracking-wider font-semibold rounded-lg' >Save</button>
+        </div>
+      </section>
+
+      <section className='bg-white w-11/12 mx-auto rounded-lg mb-5 overflow-hidden'>
+        <table className='w-full text-center'>
+          <thead className='bg-gray-100 text-sm uppercase tracking-wide text-gray-700'>
+            <tr>
+              <th className='p-3'>Id</th>
+              <th className='p-3'>Name</th>
+              <th className='p-3'>Categories</th>
+              <th className='p-3'>Price</th>
+              <th className='p-3'>More Details</th>
+              <th className='p-3'>Action</th>
+            </tr>
+          </thead>
+          <tbody className='divide-y divide-gray-200 text-sm'>
+            <tr className='hover:bg-gray-50'>
+              <td className='p-3'>1</td>
+              <td className='p-3 font-medium text-gray-800'>product1</td>
+              <td className='p-3'>Best Sellers</td>
+              <td className='p-3'>678</td>
+              <td className='p-3'>
+
+                {
+                  !open? 
+                   <button className='mx-auto flex h-8 w-8 items-center justify-center rounded-full bg-gray-100 text-gray-700 hover:bg-gray-200' 
+                onClick={()=>setOpen(true) }>
+                  <FaChevronDown  />
+                </button>
+                :
+                 <button className='mx-auto flex h-8 w-8 items-center justify-center rounded-full bg-gray-100 text-gray-700 hover:bg-gray-200' 
+                onClick={()=>setOpen( false) }>
+                  <FaChevronUp  />
+                </button>
+                }
+              </td>
+              <td className='p-3'>
+                <div className='flex items-center justify-center gap-2'>
+                  <EditButton btn="Edit" />
+                  <DeleteButton deleteBtn="Delete" />
+                </div>
+              </td>
+            </tr>
+
+           {open &&  <tr>
+              <td>Discount</td>
+              <td>Description</td>
+              <td>Stocks</td>
+              <td>SKU</td>
+              <td>Variant</td>
+              <td>Images</td>
+            </tr>}
+          </tbody>
+        </table>
       </section>
     </>
   )
