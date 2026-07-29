7d3f388bd541082dfa0c718424e60c5cf635a9ee
manpreet-codespace <manpreet.dev.85@gmail.com>
2026-07-30 01:14:14 +0530

    updated: not found

--- frontend/src/NotFoundPage.jsx
+++ frontend/src/NotFoundPage.jsx
@@ -1,14 +1,17 @@
 import React from 'react'
 import { FaHome } from "react-icons/fa";
+import {useNavigate} from "react-router-dom";
 
 const NotFoundPage = () => {
+    const navigate = useNavigate();
+
   return (
     <>
       <div className='bg-blue-600 h-100 text-center text-white py-10'>
         <p className='font-bold text-[18px]'>We’re sorry — something has gone wrong on our end.</p>
         <p>If difficulties persist, please contact the System Administrator of this site and report the error below..</p>
 
-        <button className='flex gap-3 items-center mx-auto my-10 bg-green-600 px-7 py-5 rounded-4xl'>
+        <button className='flex gap-3 items-center mx-auto my-10 bg-green-600 px-7 py-5 rounded-4xl' onClick={()=>navigate("/")}>
           <FaHome/> Back to Homepage
         </button>
       </div>
